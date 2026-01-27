e562182fb258f2caa29e86e9b2615e9a73a43f45
SpyrosGew <spyrosgewdark@gmail.com>
2026-01-27 03:51:38 +0200

    small fix

--- main.py
+++ main.py
@@ -47,9 +47,7 @@ def main():
     print("\n=== GCN LINK PREDICTION ===")
 
     gcn_model = train_gcn_link_predictor(dataset)
-    auc_gcn = evaluate_gcn(gcn_model, dataset)
-
-    print(f"AUC : {auc_gcn:.4f}")
+    evaluate_gcn(gcn_model, dataset)
 
 
 if __name__ == "__main__":
